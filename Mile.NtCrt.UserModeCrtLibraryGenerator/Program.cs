557b8987437f342691298c9790218546ba7855e3
MouriNaruto <Kenji.Mouri@outlook.com>
2024-12-29 19:18:17 +0800

    Create Mile.NtCrt.UserModeCrtLibraryGenerator project.

---
+++ Mile.NtCrt.UserModeCrtLibraryGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Mile.NtCrt.UserModeCrtLibraryGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "{0} task has been completed.",
+                "Mile.NtCrt.UserModeCrtLibraryGenerator");
+            Console.ReadKey();
+        }
+    }
+}
