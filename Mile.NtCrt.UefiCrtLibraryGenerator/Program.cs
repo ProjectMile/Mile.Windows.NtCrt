264c2e056eedb78388c884218bb699ade8471a12
MouriNaruto <Kenji.Mouri@outlook.com>
2024-12-29 12:43:49 +0800

    Add Mile.NtCrt.UefiCrtLibraryGenerator project.

---
+++ Mile.NtCrt.UefiCrtLibraryGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Mile.NtCrt.UefiCrtLibraryGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "{0} task has been completed.",
+                "Mile.NtCrt.UefiCrtLibraryGenerator");
+            Console.ReadKey();
+        }
+    }
+}
