20b29f26416c40a2202e8cc104ec75c9a4e53015
MouriNaruto <Kenji.Mouri@outlook.com>
2024-12-29 19:19:40 +0800

    Install packages to Mile.NtCrt.UserModeCrtLibraryGenerator project.

--- Mile.NtCrt.UserModeCrtLibraryGenerator/Program.cs
+++ Mile.NtCrt.UserModeCrtLibraryGenerator/Program.cs
@@ -1,7 +1,15 @@
-﻿namespace Mile.NtCrt.UserModeCrtLibraryGenerator
+﻿using Mile.Project.Helpers;
+
+namespace Mile.NtCrt.UserModeCrtLibraryGenerator
 {
     internal class Program
     {
+        private static readonly string ProjectRootPath =
+           GitRepository.GetRootPath();
+
+        private static readonly string ReferencesRootPath =
+            ProjectRootPath + @"\Mile.NtCrt.References";
+
         static void Main(string[] args)
         {
             Console.WriteLine(
