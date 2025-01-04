cd2bb45f11dfe9022129fbbebfc7a32cc0a07ad8
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-04 20:07:24 +0800

    Create Mile.NtCrt.Uefi project.

---
+++ Mile.NtCrt.Uefi/Mile.NtCrt.Uefi.Stubs.cpp
@@ -0,0 +1,9 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.NtCrt.Uefi.Stubs.cpp
+ * PURPOSE:   Stubs for Windows Driver Kit C Runtime for UEFI
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
---
+++ Mile.NtCrt.Uefi/Mile.NtCrt.Uefi.Test.cpp
@@ -0,0 +1,35 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.NtCrt.Uefi.Test.cpp
+ * PURPOSE:   Linking Test for Windows Driver Kit C Runtime for UEFI
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+BOOL WINAPI DllMain(
+    _In_ HINSTANCE hinstDLL,
+    _In_ DWORD fdwReason,
+    _In_ LPVOID lpvReserved)
+{
+    UNREFERENCED_PARAMETER(hinstDLL);
+    UNREFERENCED_PARAMETER(lpvReserved);
+
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+#pragma comment(linker, "/entry:DllMain")
