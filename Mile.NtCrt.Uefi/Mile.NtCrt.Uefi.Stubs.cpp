fb7764a38827dfcf019c6fea8e21064c5046a25e
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-05 21:35:41 +0800

    Continue to add objects from libcntpr to Mile.NtCrt.Uefi.

--- Mile.NtCrt.Uefi/Mile.NtCrt.Uefi.Stubs.cpp
+++ Mile.NtCrt.Uefi/Mile.NtCrt.Uefi.Stubs.cpp
@@ -52,3 +52,44 @@ extern "C" __declspec(noreturn) void __cdecl __report_gsfailure(
     stack_cookie;
     for (;;) {}
 }
+
+__pragma(comment(linker, "/alternatename:__lookuptable_s=__lookuptable"));
+
+#ifndef _SIZE_T_DEFINED
+#ifdef  _WIN64
+typedef unsigned __int64 size_t;
+#else
+typedef _W64 unsigned int size_t;
+#endif
+#define _SIZE_T_DEFINED
+#endif
+
+typedef int errno_t;
+
+#ifndef EILSEQ
+#define EILSEQ 42
+#endif // !EILSEQ
+
+extern "C" errno_t __cdecl wctomb_s(
+    int* const return_value,
+    char* const destination,
+    size_t const destination_count,
+    wchar_t const wchar)
+{
+    return_value;
+    destination;
+    destination_count;
+    wchar;
+    return EILSEQ;
+}
+
+extern "C" int __cdecl mbtowc(
+    wchar_t* pwc,
+    const char* s,
+    size_t n)
+{
+    pwc;
+    s;
+    n;
+    return -1;
+}
