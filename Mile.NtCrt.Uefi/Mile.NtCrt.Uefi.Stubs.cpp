70e1c8432184ef5dba783d328721f452e47fa1a9
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-05 11:17:52 +0800

    Start to add objects from libcntpr to Mile.NtCrt.Uefi.

--- Mile.NtCrt.Uefi/Mile.NtCrt.Uefi.Stubs.cpp
+++ Mile.NtCrt.Uefi/Mile.NtCrt.Uefi.Stubs.cpp
@@ -8,9 +8,16 @@
  * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
  */
 
+#include <stdint.h>
+
 namespace
 {
     int g_errno = 0;
+}
+
+extern "C" int* __cdecl _errno()
+{
+    return &g_errno;
 }
 
 extern "C" void __cdecl _invalid_parameter_noinfo()
@@ -18,7 +25,16 @@ extern "C" void __cdecl _invalid_parameter_noinfo()
     // Do nothing.
 }
 
-extern "C" int* __cdecl _errno()
+extern "C" void __cdecl _invalid_parameter(
+    wchar_t const* const expression,
+    wchar_t const* const function_name,
+    wchar_t const* const file_name,
+    unsigned int const line_number,
+    uintptr_t const reserved)
 {
-    return &g_errno;
+    expression;
+    function_name;
+    file_name;
+    line_number;
+    reserved;
 }
